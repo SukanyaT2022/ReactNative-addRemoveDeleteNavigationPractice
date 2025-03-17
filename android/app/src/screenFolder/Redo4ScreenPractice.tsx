b86f5589f42a75ac7df56358d2f9e0e5c3470a43
yoon <sukanyat2022a@gmail.com>
2025-03-17 13:57:03 -0400

    add delete

--- android/app/src/screenFolder/Redo4ScreenPractice.tsx
+++ android/app/src/screenFolder/Redo4ScreenPractice.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {use, useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 const Redo4ScreenPractice = () => {
   const [carArray, setCarArray] = useState<string[]>([
@@ -14,6 +14,30 @@ const Redo4ScreenPractice = () => {
     console.log(item);
   };
 
+  const [holdDeleteCarType, setholdDeleteCarType] = useState<string[]>([]);
+  const deleteFunc = (item: string) => {
+    setholdDeleteCarType(prevState => {
+      //when e want to delete -check if car we want to delete exist-.some use to check item there
+      let check: boolean = prevState.some(car => item == car);
+      if (check) {
+        return prevState;
+      } else {
+        return [...prevState, item];
+      }
+    });
+setCarArray((prevState)=>prevState.filter((value)=>value !== item))
+  };
+const refreshBtnFunc =()=>{
+setCarArray((prevState)=>{
+let check:boolean = holdDeleteCarType.some((item)=>prevState.includes(item))
+if (check) {
+  return prevState;
+} else {
+  return [...prevState, ...holdDeleteCarType];
+}
+})
+}
+
   return (
     <View>
       <Text>Redo4ScreenPractice</Text>
@@ -23,18 +47,24 @@ const Redo4ScreenPractice = () => {
             <TouchableOpacity
               key={index}
               onPress={() => selectFunc(item)}
-              style={[styles.mainBoxStyle, selectCar == item && styles.changeColorSelect]}>
+              style={[
+                styles.mainBoxStyle,
+                selectCar == item && styles.changeColorSelect,
+              ]}>
               <Text>{item}</Text>
               <Icon
                 name="plus"
                 size={10}
                 color="#900"
                 // style={{marginTop: 100}}
+                onPress={() => deleteFunc(item)}
               />
             </TouchableOpacity>
+
           );
         })}
       </View>
+      <TouchableOpacity onPress={()=>refreshBtnFunc()} style={{backgroundColor:'green'}}><Text>Refresh Btn</Text></TouchableOpacity>
     </View>
   );
 };
